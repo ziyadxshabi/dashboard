3c6f236f42ba98f61e2c3038165405e437cf14ab
zvdzad <ziyadxshabi@gmail.com>
2026-09-02 10:35:28 +0100

    phase-2.1: remove ngrok header from bulk-sms.js

--- Temara_Dashboard/api/bulk-sms.js
+++ Temara_Dashboard/api/bulk-sms.js
@@ -37,7 +37,6 @@ async function handler(req, res) {
   const headers = {
     accept: 'application/json',
     'content-type': 'application/json',
-    'ngrok-skip-browser-warning': 'true',
     'user-agent': 'DentaFlow-Doctor-Proxy/1.0',
     'x-agency-auth': authKey,
   };
